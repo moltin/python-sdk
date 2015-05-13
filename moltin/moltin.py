9a0a755d4d7d3ae871dd73f6fc2aba01165e0f5d
Robin Bilgil <robin.bilgil@gmail.com>
2015-05-13 16:25:14 +0100

    added authentication flow to Moltin class

--- moltin/moltin.py
+++ moltin/moltin.py
@@ -12,4 +12,17 @@ class Moltin:
         self.authenticator = Authenticator(client_id, client_secret)
         self.auth_result = None
 
+    def authenticate(self, refresh_token=None, username=None, password=None):
+
+        if refresh_token is not None:
+            self.auth_result = self.authenticator.with_refresh(refresh_token)
+
+        elif username is not None or password is not None:
+            # Make sure we have both user and pass
+            if username is None or password is None:
+                raise AuthError("Both username and password is required for password authentication")
+
+            self.auth_result = self.authenticator.with_password(username, password)
+        else:
+            self.auth_result = self.authenticator.with_client_credentials()
 
