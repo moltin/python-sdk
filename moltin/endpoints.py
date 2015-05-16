2ed90596786078ec5fa0812f68148272818ec983
Robin Bilgil <robin.bilgil@gmail.com>
2015-05-16 12:46:33 +0100

    created base endpoint for API

---
+++ moltin/endpoints.py
@@ -0,0 +1,20 @@
+from moltin.request import Request
+
+
+class BaseEndpoint(Request):
+
+    def list(self):
+        pass
+
+    def find(self):
+        pass
+
+    def create(self):
+        pass
+
+    def update(self):
+        pass
+
+    def delete(self):
+        pass
+
