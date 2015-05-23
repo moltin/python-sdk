378f35e696a7d0453659bb3499431392160866ee
Robin Bilgil <robin.bilgil@gmail.com>
2015-05-23 12:36:19 +0100

    testing successful response

--- moltin/tests/unit/test_request.py
+++ moltin/tests/unit/test_request.py
@@ -30,3 +30,9 @@ def test_request_with_invalid_code(mock_get):
     mock_get.return_value = create_mock_response({"error": "Invalid Auth Code"})
     set_access_token("somestring", time() + 3600)
     r.get.when.called_with('products/').should.throw(RequestError)
+
+@mock.patch("moltin.requests.get")
+def test_request_with_invalid_code(mock_get):
+    mock_get.return_value = create_mock_response({"products": []})
+    response = r.get('products/')
+    expect(response["products"]).to.eql([])
